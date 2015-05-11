d8e7e1e81503e65f174fe5e32e4c1fbf12f9b106
Pedro Dias <petermdias@gmail.com>
2015-05-11 21:03:15 +0100

    tweaks

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -1,6 +1,6 @@
 window.HomeView = Backbone.View.extend({
 
-  initialize:function () {
+  initialize: function() {
     this.render();
   },
 
@@ -9,14 +9,16 @@ window.HomeView = Backbone.View.extend({
   },
 
   submit_handler: function() {
-    console.log('ping');
     var module = $("#module").val();
-    app.navigate('info/' + module, {trigger: true});
+    app.navigate('info/' + module, {
+      trigger: true
+    });
     return false;
   },
 
-  render:function () {
+  render: function() {
     $(this.el).html(this.template());
+    $('.loader').show();
 
     $.getJSON("/api/stats", function(data) {
       var total = parseInt(data.ok) + parseInt(data.nok) + parseInt(data.timedout) + parseInt(data.nottested);
@@ -31,6 +33,14 @@ window.HomeView = Backbone.View.extend({
       $('#percentage_timedout', this.el).html(((data.timedout / total) * 100).toFixed(0) + '%');
       $('#digit_nottested', this.el).html(data.nottested);
       $('#percentage_nottested', this.el).html(((data.nottested / total) * 100).toFixed(0) + '%');
+
+      $('#stats_tested', this.el).html((((data.ok + data.nok) / total) * 100).toFixed(0) + '%');
+      $('#stats_untested', this.el).html((((data.nottested) / total) * 100).toFixed(0) + '%');
+      $('#stats_queue', this.el).html(data.null);
+      $('#stats_published', this.el).html(data.null + data.failed + data.timedout + data.nok + data.ok + data.nottested);
+      $('#stats_doe', this.el).html((((data.failed + data.timedout) / total) * 100).toFixed(0) + '%');
+
+      $('.loader').hide();
     });
 
     return this;
